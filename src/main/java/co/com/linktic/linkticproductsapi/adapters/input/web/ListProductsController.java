6fbd4c6675001d3d9c0fb6f1f64d37e964c67793
Andres Leal <andres.leal@MacBook-Pro-de-Andres.local>
2024-09-09 09:12:16 -0500

    Adds CrossOrigin and license

--- src/main/java/co/com/linktic/linkticproductsapi/adapters/input/web/ListProductsController.java
+++ src/main/java/co/com/linktic/linkticproductsapi/adapters/input/web/ListProductsController.java
@@ -2,8 +2,11 @@ package co.com.linktic.linkticproductsapi.adapters.input.web;
 
 import co.com.linktic.linkticproductsapi.adapters.input.web.dto.ProductDto;
 import co.com.linktic.linkticproductsapi.domain.usecases.ListProductsUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +16,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@Tag(name = "Products", description = "API for managing products")
 public class ListProductsController {
 
     private final ListProductsUseCase listProductsUseCase;
 
     @GetMapping("/products")
+    @Operation(summary = "List products", description = "Returns a list of products.")
     public ResponseEntity<List<ProductDto>> listProducts() {
         List<ProductDto> productDtos = listProductsUseCase.listProducts()
                 .stream().map(product -> new ProductDto(
